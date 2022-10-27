4b341edac3ff5869da6683bbc363339180f6ccf9
inayatullahaaxioned <inayatullaha@axioned.com>
2022-10-27 19:33:28 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-/* Author: 
-
+/* Author:
+Inayatullah
 */
+
+const slider = document.getElementById("slider"),
+  label = document.querySelector("label");
+
+label.innerText = slider.value;
+label.style.left = slider.value + "%";
+
+slider.addEventListener('input', function () {
+  label.innerText = slider.value;
+  label.style.left = slider.value + "%";
+});
 
 
 
